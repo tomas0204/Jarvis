95b87c42c53fcdb5741df061ed3c785ed606473e
Tomás <tomasignacioponce17@gmail.com>
2026-08-16 21:55:16 -0300

    feat: implementacion de endpoint POST /message

--- backend/api/app.py
+++ backend/api/app.py
@@ -1,3 +1,13 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
+
+class MessageRequest(BaseModel):
+    text: str
+    
+@app.post("/api/message")
+def send_message(message: MessageRequest):
+    return {
+        "response": f"Recibí: {message.text}"
+    }
