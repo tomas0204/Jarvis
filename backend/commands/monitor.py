09a121579bbfedc1fe6272800f059fd4651dc73a
Tomás <tomasignacioponce17@gmail.com>
2026-08-19 20:56:03 -0300

    feat: monitor.py extrae datos de la ram

--- backend/commands/monitor.py
+++ backend/commands/monitor.py
@@ -5,3 +5,6 @@ class SystemMonitor:
 
     def get_cpu_usage(self) -> float:
         return psutil.cpu_percent(interval=0.5)
+
+    def get_memory_usage(self) -> float:
+        return psutil.virtual_memory().percent
--- test_monitor.py
+++ test_monitor.py
@@ -3,3 +3,4 @@ from backend.commands.monitor import SystemMonitor
 monitor = SystemMonitor()
 
 print(monitor.get_cpu_usage())
+print(monitor.get_memory_usage())
