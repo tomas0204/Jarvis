b22831d2df323fb62ee154583d7d6a9a7bbfe411
Tomás <tomasignacioponce17@gmail.com>
2026-08-29 20:47:59 -0300

    feat: se agregó la aplicacion VS Code

--- config.py
+++ config.py
@@ -47,6 +47,11 @@ APPLICATIONS = {
         "value": r"C:\Users\tomas\AppData\Local\Discord\app-1.0.9255\Discord.exe"
     },
     
+    "visual": {
+        "type": "path",
+        "value": r"C:\Users\tomas\AppData\Local\Programs\Microsoft VS Code\Code.exe"
+    },
+    
     "spotify": {
         "type": "uri",
         "value": "spotify:"
